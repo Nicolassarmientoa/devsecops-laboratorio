354fdf01720432f87bec0949e13605d8752a6205
NicoSarmi <nsarmi@gmail.com>
2025-10-27 20:36:53 -0300

    test: introduce insecure pattern to trigger SAST

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,13 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+
+// WARNING: Hardcoded secret for testing Sonar
+const HARD_CODED_API_KEY = "sk_test_1234567890_DO_NOT_DO_THIS";
+
+// Insecure dynamic eval usage
+function insecureEval(userInput: string) {
+  // Esto es inseguro a propósito para gatillar findings
+  return eval(userInput);
+}
